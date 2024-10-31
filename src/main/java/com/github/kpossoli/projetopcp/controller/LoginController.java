d9dd6ec2343e3a48fe8d297110cc38283128d05e
antonioszm <antonioszm@outlook.com>
2024-10-30 23:27:51 -0300

    feat: Documentacao dos endpoints do LoginController

--- src/main/java/com/github/kpossoli/projetopcp/controller/LoginController.java
+++ src/main/java/com/github/kpossoli/projetopcp/controller/LoginController.java
@@ -4,6 +4,11 @@ import com.github.kpossoli.projetopcp.security.Autorizacao;
 import com.github.kpossoli.projetopcp.security.RespostaJWT;
 import com.github.kpossoli.projetopcp.security.JwtService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,6 +25,27 @@ public class LoginController {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
+    @Operation(summary = "Gera um token de login ao usuario no sistema", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login bem-sucedido. Retorna um token JWT (JSON Web Token) no corpo da resposta.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(example = "{ \"accessToken\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...\" }")
+            )),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas. O usuário não está autorizado a acessar o sistema.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    example = "{ \"error\": \"Unauthorized\", \"message\": \"Credenciais inválidas.\" }"
+            ))),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida, dados ausentes ou incorretos.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    type = "object",
+                                    example = "{ \"status\": 400, \"messages\": [{ \"code\": \"login\", \"message\": \"Usuário ou senha inválidos\" }] }"
+            )))
+    })
     @PostMapping
     public RespostaJWT login(@RequestBody Autorizacao authRequestDTO){
         var auth = new UsernamePasswordAuthenticationToken(authRequestDTO.getEmail(), authRequestDTO.getSenha());
@@ -33,5 +59,4 @@ public class LoginController {
             throw new UsernameNotFoundException("Usuário Não encontrado!");
         }
     }
-
 }
