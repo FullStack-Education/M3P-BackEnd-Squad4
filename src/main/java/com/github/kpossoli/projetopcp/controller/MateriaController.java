6dd4da1a3979cb69aa6bba3c265901d559b85414
antonioszm <antonioszm@outlook.com>
2024-10-31 22:47:01 -0300

    feat: documentacaos dos endpoints de MateriaController

--- src/main/java/com/github/kpossoli/projetopcp/controller/MateriaController.java
+++ src/main/java/com/github/kpossoli/projetopcp/controller/MateriaController.java
@@ -1,6 +1,11 @@
 package com.github.kpossoli.projetopcp.controller;
 
 import com.github.kpossoli.projetopcp.service.CursoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import com.github.kpossoli.projetopcp.dto.MateriaDto;
@@ -26,6 +31,29 @@ public class MateriaController {
     private final MateriaMapper materiaMapper;
     private final CursoService cursoService;
 
+    @Operation(summary = "Realiza a busca da Materia pelo ID", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Materia encontrada com sucesso.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    example = "{\n" +
+                                            "  \"id\": 1,\n" +
+                                            "  \"nome\": \"Java\"\n" +
+                                            "}"
+                            )
+                    )),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas. O usuário não está autorizado a acessar o sistema.",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "404", description = "Materia não encontrada.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    type = "object",
+                                    example = "{ \"status\": 404, \"messages\": [{ \"code\": \"not-found\", \"message\": \"Recurso não encontrado\" }] }"
+                            )))
+    })
     @GetMapping("materias/{id}")
     @PreAuthorize("hasAuthority('MATERIA_READ')")
     public ResponseEntity<MateriaDto> obter(@PathVariable Long id) {
@@ -35,6 +63,29 @@ public class MateriaController {
         return ResponseEntity.ok(materiaDto);
     }
 
+    @Operation(summary = "Retorna todas as Materia cadastradas", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Materia encontradas com sucesso.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    example = "[\n"+ "{\n" +
+                                            "  \"id\": 1,\n" +
+                                            "  \"nome\": \"Java\"\n" +
+                                            "}" + "]"
+                            )
+                    )),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas. O usuário não está autorizado a acessar o sistema.",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "404", description = "Não há Materia cadastrados",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    type = "object",
+                                    example = "{ \"status\": 404, \"messages\": [{ \"code\": \"not-found\", \"message\": \"Recurso não encontrado\" }] }"
+                            )))
+    })
     @GetMapping("materias")
     @PreAuthorize("hasAuthority('MATERIA_READ')")
     public ResponseEntity<List<MateriaDto>> listar() {
@@ -44,6 +95,29 @@ public class MateriaController {
         return ResponseEntity.ok(materiasDto);
     }
 
+    @Operation(summary = "Realiza a busca da Materia pelo ID do Curso", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Materia encontrada com sucesso.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    example = "{\n" +
+                                            "  \"id\": 1,\n" +
+                                            "  \"nome\": \"Java\"\n" +
+                                            "}"
+                            )
+                    )),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas. O usuário não está autorizado a acessar o sistema.",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "404", description = "Curso não encontrado.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    type = "object",
+                                    example = "{ \"status\": 404, \"messages\": [{ \"code\": \"not-found\", \"message\": \"Recurso não encontrado\" }] }"
+                            )))
+    })
     @GetMapping("cursos/{id}/materias")
     @PreAuthorize("hasAuthority('MATERIA_READ')")
     public ResponseEntity<List<MateriaDto>> listarMateriasPorCurso(@PathVariable("id") Long idCurso) {
@@ -53,6 +127,29 @@ public class MateriaController {
         return ResponseEntity.ok(materiasDto);
     }
 
+    @Operation(summary = "Cadastra Materia no sistema", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Materia criada com sucesso.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    example = "{\n" +
+                                            "  \"id\": 1,\n" +
+                                            "  \"nome\": \"Java\"\n" +
+                                            "}"
+                            )
+                    )),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas. O usuário não está autorizado a acessar o sistema.",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida, por exemplo, dados ausentes ou incorretos.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    type = "object",
+                                    example = "{ \"status\": 400, \"messages\": [{ \"code\": \"json_parse\", \"message\": \"Mensagem inválida\" }] }"
+                            )))
+    })
     @PostMapping("materias")
     @PreAuthorize("hasAuthority('MATERIA_WRITE')")
     public ResponseEntity<MateriaDto> criar(@RequestBody @Valid MateriaDto materiaDto) {
@@ -63,6 +160,28 @@ public class MateriaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(materiaSalvoDto);
     }
 
+    @Operation(summary = "Atualiza a Materia pelo ID", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Materia atualizada com sucesso.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    example = "{\n" +
+                                            "  \"id\": 1,\n" +
+                                            "  \"nome\": \"Java\"\n" +
+                                            "}"
+                            ))),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas. O usuário não está autorizado a acessar o sistema.",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "404", description = "Materia não encontrada.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    type = "object",
+                                    example = "{ \"status\": 404, \"messages\": [{ \"code\": \"not-found\", \"message\": \"Recurso não encontrado\" }] }"
+                            )))
+    })
     @PutMapping("materias/{id}")
     @PreAuthorize("hasAuthority('MATERIA_WRITE')")
     public ResponseEntity<MateriaDto> atualizar(@PathVariable Long id, @RequestBody @Valid MateriaDto materiaDto) {
@@ -73,6 +192,22 @@ public class MateriaController {
         return ResponseEntity.ok(materiaSalvoDto);
     }
 
+    @Operation(summary = "Exclui Materia pelo ID", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Materia excluida com sucesso.",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas. O usuário não está autorizado a acessar o sistema.",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "404", description = "Materia não encontrada.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    type = "object",
+                                    example = "{ \"status\": 404, \"messages\": [{ \"code\": \"not-found\", \"message\": \"Recurso não encontrado\" }] }"
+                            )))
+    })
     @DeleteMapping("materias/{id}")
     @PreAuthorize("hasAuthority('MATERIA_DELETE')")
     public ResponseEntity<Void> excluir(@PathVariable Long id) {
